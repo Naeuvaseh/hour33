82fb8150d9b37b25f3ee0ace4c323c2b0ca635cb
TJ Seaman <naeuvaseh@gmail.com>
2018-04-01 18:54:22 -0400

    Users can now login, create an account, logout, and reset their password.

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Theme } from '../../settings';
 import { errorHandler } from '@angular/platform-browser/src/browser';
 import { Router } from '@angular/router';
 import { LoginState } from '../../enums/login-state.enum';
+import { UserService } from '../../services/user.service';
 
 const firebase = require('nativescript-plugin-firebase');
 
@@ -10,26 +11,44 @@ const firebase = require('nativescript-plugin-firebase');
   selector: 'login',
   templateUrl: './components/login/login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     private theme;
-    public user = {
-        email: '',
-        password: '',
-        confirmPassword: ''
-    }
+    public title;
+    public loginStates = LoginState;
     public loginState: string = LoginState.Login;
   
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private userService: UserService) {
       this.theme = Theme;
+      this.title = 'Login';
     }
 
-    toggleForgotPassword(){
-        this.loginState = LoginState.ForgotPassword;
+    ngOnInit(){
+        console.log('LoginComponent()');
+        if (this.userService.user){
+            this.router.navigate(['/search']);
+        }
     }
 
-    toggleLogin(){
-        this.loginState = LoginState.Login;
+    toggleView(action: string){
+        switch(action){
+            case LoginState.Login: {
+                this.title = 'Login'
+                this.loginState = LoginState.Login;
+            }
+            break;
+            case LoginState.ForgotPassword: {
+                this.title = 'Forgot Password?'
+                this.loginState = LoginState.ForgotPassword;
+            }
+            break;
+            case LoginState.CreateAccount: {
+                this.title = 'Create an Account';
+                this.loginState = LoginState.CreateAccount;
+            }
+        }
+        
     }
 
     resetPassword(email: string){
@@ -48,27 +67,19 @@ export class LoginComponent {
           );
     }
 
-    toggleCreateAccount(){
-        this.loginState = LoginState.CreateAccount;
-    }
-
     onCancel(){
-        
+        this.router.navigate(['/search']);
     }
 
     onCreateAccount(email: string, password: string, confirmPassword: string){
         if (email){
             if (password == confirmPassword){
                 firebase.createUser({
-                    email: this.user.email,
-                    password: this.user.password
+                    email: email,
+                    password: password
                   }).then(
                       function (result) {
-                        alert({
-                          title: "User created",
-                          message: "userid: " + result.key,
-                          okButtonText: "Nice!"
-                        })
+                       this.router.navigate(['/search']);
                       },
                       function (errorMessage) {
                         alert({
@@ -88,85 +99,84 @@ export class LoginComponent {
         }
     }
 
-    onLogin(){
+    onLogin(email: string, password: string){
         var router = this.router;
-        var user = this.user;
-        console.log('User Email: ' + this.user.email + '; Password: ' + this.user.password);
-        if(this.user.email == null || this.user.email == ''){
-        alert({
-            title: "Missing email",
-            message: "Please enter an email address.",
-            okButtonText: "Ok"
-        });
-        }
-        else if (this.user.password == null || this.user.password == ''){
-        alert({
-            title: "Missing password",
-            message: "Please enter a password.",
-            okButtonText: "Ok"
-        });
-        }
-        else {
-        firebase.login({
-            type: firebase.LoginType.PASSWORD,
-            passwordOptions: {
-            email: this.user.email,
-            password: this.user.password
-            }
-        }).then(
-            // Success
-            function (result) {
-            JSON.stringify(result);
-            //Redirect to Search screen
-            router.navigate(["/search"]);
-            },
-            // Error
-            function (errorMessage) {
-            console.log(errorMessage);
-            // Login Failed
-            if (errorMessage.match("The password is invalid or the user does not have a password.")) {
-                alert({
-                title: "An error occurred",
-                message: "The password is invalid or the user does not have a password.",
+        // Check if email is null or blank
+        if(email == null || email == ''){
+            alert({
+                title: "Missing email",
+                message: "Please enter an email address.",
                 okButtonText: "Ok"
-                });
-            }
-            // User doesn't exist
-            if (errorMessage.match("There is no user record corresponding to this identifier. The user may have been deleted.")) {
-                // Create User
-                firebase.createUser({
-                email: this.user.email,
-                password: this.user.password
-                }).then(
+            });
+        }
+        if (password == null || password == ''){
+            alert({
+                title: "Missing password",
+                message: "Please enter a password.",
+                okButtonText: "Ok"
+            });
+        }
+        if ((email != null || email != '') && (password != null || password != '')) {
+            firebase.login({
+                type: firebase.LoginType.PASSWORD,
+                passwordOptions: {
+                    email: email,
+                    password: password
+                }
+            })
+            .then(
+                // Success
                 function (result) {
-                    alert({
-                    title: "User created sucessfully",
-                    message: "Congratulations! Happy Drinking!",
-                    okButtonText: "Sweet!"
-                    });
-                    // Send confirmation email
-                    firebase.sendEmailVerification().then(
-                    function () {
-                        console.log("Email verification sent");
-                    },
-                    function (error) {
-                        console.log("Error sending email verification: " + error);
-                    }
-                    );
+                    console.log(JSON.stringify(result));
                     //Redirect to Search screen
                     router.navigate(["/search"]);
                 },
+                // Error
                 function (errorMessage) {
-                    alert({
-                        title: "No user created",
-                        message: errorMessage,
-                        okButtonText: "OK, got it"
-                    });
-                    }
-                );
-            }
-            }
-            );
+                    console.log(errorMessage);
+                    // Login Failed
+                    if (errorMessage.match("The password is invalid or the user does not have a password.")) {
+                        alert({
+                        title: "An error occurred",
+                        message: "The password is invalid or the user does not have a password.",
+                        okButtonText: "Ok"
+                        });
+                }
+                // User doesn't exist
+                if (errorMessage.match("There is no user record corresponding to this identifier. The user may have been deleted.")) {
+                    // Create User
+                    firebase.createUser({
+                    email: this.user.email,
+                    password: this.user.password
+                    }).then(
+                    function (result) {
+                        alert({
+                        title: "User created sucessfully",
+                        message: "Congratulations! Happy Drinking!",
+                        okButtonText: "Sweet!"
+                        });
+                        // Send confirmation email
+                        firebase.sendEmailVerification().then(
+                        function () {
+                            console.log("Email verification sent");
+                        },
+                        function (error) {
+                            console.log("Error sending email verification: " + error);
+                        }
+                        );
+                        //Redirect to Search screen
+                        router.navigate(["/search"]);
+                    },
+                    function (errorMessage) {
+                        alert({
+                            title: "No user created",
+                            message: errorMessage,
+                            okButtonText: "OK, got it"
+                        });
+                        }
+                    );
+                }
+            });
         }
     }
 
