3afb521ee1fc7710b37f52f559ff33e1755985f1
TJ Seaman <naeuvaseh@gmail.com>
2018-04-09 14:16:57 -0400

    New branch

--- platforms/android/app/build/intermediates/assets/F0F1F2F3F4F5F6F7/debug/app/services/user.service.ts
+++ platforms/android/app/build/intermediates/assets/F0F1F2F3F4F5F6F7/debug/app/services/user.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
 const firebase = require("nativescript-plugin-firebase");
 
 @Injectable()
 export class UserService {
-  private _user: any;
+  private _user: Observable<any>;
+  loggedIn: boolean;
 
   constructor() {
     firebase
       .getCurrentUser()
       .then(user => {
-        console.log("User: " + JSON.stringify(user));
-        this._user = user;
+        if (user) {
+          console.log("User: " + JSON.stringify(user));
+          this._user = user;
+          this.loggedIn = true;
+        } else {
+          this._user = null;
+          this.loggedIn = false;
+        }
       })
-      .catch(error => console.log("Trouble in paradise: " + error));
+      .catch(error =>
+        console.log("UserService.constructor():[ERROR] " + error)
+      );
   }
 
-  get user() {
+  getUser(): Observable<any> {
     console.log("Getting current user: " + JSON.stringify(this._user));
     return this._user;
+  }
+
+  setUset(user: any) {
+    this._user = user;
+  }
+
+  logout() {
+    firebase.logout().then(success => {
+      this._user = null;
+      this.loggedIn = false;
+    });
   }
 }
